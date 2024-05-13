1c99539fdf4840e25ab3fb842e66dbc65efdb87e
Samuel Zeray <samuelzeray09@gmail.com>
2024-05-13 09:30:04 -0200

    adding 3 junit tests for 3 functions in calculator.java For 1) Factorial 2) Permutation 3) Combination

--- Test.java
+++ Test.java
@@ -2,11 +2,82 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Test {
+     Calculator p= new Calculator();
+
+   
+   // Factorial
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, p.calculateFactorial(0));
+    }
     
     @Test
-    public void testpower (){
-        Calculator p= new Calculator();
-         assertEquals(8.0, p.power(2.0,3.0));
+    public void testFactorialOfOne() {
+        assertEquals(1, p.calculateFactorial(1));
+    }
+    
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(120, p.calculateFactorial(5));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        p.calculateFactorial(-5);
+    }
+
+
+           // Permutation    
+    @Test
+    public void testPermutationOfTwoNumbers() {
+        assertEquals(6, p.calculatePermutation(3, 2));
+    }
+    
+    @Test
+    public void testPermutationWithSameNumbers() {
+        assertEquals(1, p.calculatePermutation(5, 5));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testPermutationWithNegativeNumberN() {
+        p.calculatePermutation(-3, 2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testPermutationWithNegativeNumberR() {
+        p.calculatePermutation(3, -2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testPermutationWithRGreaterThanN() {
+        p.calculatePermutation(2, 3);
+    }
+
+            // Combination
+
+    @Test
+    public void testCombinationOfTwoNumbers() {
+        assertEquals(3, p.calculateCombination(3, 2));
+    }
+    
+    @Test
+    public void testCombinationWithSameNumbers() {
+        assertEquals(1, p.calculateCombination(5, 5));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationWithNegativeNumberN() {
+        p.calculateCombination(-3, 2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationWithNegativeNumberR() {
+        p.calculateCombination(3, -2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testCombinationWithRGreaterThanN() {
+        p.calculateCombination(2, 3);
     }
 
 
