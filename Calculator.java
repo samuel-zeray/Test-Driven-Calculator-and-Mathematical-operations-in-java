4c0ba1aa596ba672d73d1a24974771476ad49d42
Samuel Zeray <samuelzeray09@gmail.com>
2024-05-13 21:02:24 -0200

    Adding  different types of unit conversion functions

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,20 @@
 
 public class Calculator {
-    public static int calculateaddition(int num1,num2){
+    public static int calculateaddition(int num1, int num2){
         int summation=num1+num2;
         return summation;
     }
-    public static int calculatesubtraction(int num1,num2){
+    public static int calculatesubtraction(int num1, int num2){
     int subtraction=num1-num2;
     return subtraction;
     }
-    public static int calculatemultiplication(int num1,num2){
+    public static int calculatemultiplication(int num1, int num2){
     int multiplication=num1*num2;
     return multiplication;
     }
-    public static int calculatedivision(int num1,num2){
+    public static int calculatedivision(int num1, int num2){
     int division=num1/num2;
-    return division;  
+    return division;
      }
     
     
@@ -69,6 +69,128 @@ public class Calculator {
         }
         return factorial;
     }
+       
+
+    // UNIT CONVERSION
+    
+                  //  Length Converter 
+        public static double meterToCentimeter(double meter) {
+                         return meter * 100;
+                   }
+    
+        public static double centimeterToMeter(double centimeter) {
+            return centimeter / 100;
+        }
+
+        // Area Converter
+            public static double squareMeterToSquareCentimeter(double squareMeter) {
+                return squareMeter * 10000;
+            }
+        
+            public static double squareCentimeterToSquareMeter(double squareCentimeter) {
+                return squareCentimeter / 10000;
+            }
+        
+        // Volume Converter
+                public static double cubicMeterToCubicCentimeter(double cubicMeter) {
+                    return cubicMeter * 1e6;
+                }
+            
+                public static double cubicCentimeterToCubicMeter(double cubicCentimeter) {
+                    return cubicCentimeter / 1e6;
+                }
+
+        // Mass Converter
+                    public static double kilogramToGram(double kilogram) {
+                        return kilogram * 1000;
+                    }
+                
+                    public static double gramToKilogram(double gram) {
+                        return gram / 1000;
+                    }
+
+         // Temperature Converter
+                        public static double celsiusToFahrenheit(double celsius) {
+                            return (celsius * 9 / 5) + 32;
+                        }
+                    
+                        public static double celsiusToKelvin(double celsius) {
+                            return celsius + 273.15;
+                        }
+                    
+                        public static double fahrenheitToCelsius(double fahrenheit) {
+                            return (fahrenheit - 32) * 5 / 9;
+                        }
+                    
+                        public static double fahrenheitToKelvin(double fahrenheit) {
+                            return (fahrenheit + 459.67) * 5 / 9;
+                        }
+                    
+                        public static double kelvinToCelsius(double kelvin) {
+                            return kelvin - 273.15;
+                        }
+                    
+                        public static double kelvinToFahrenheit(double kelvin) {
+                            return (kelvin * 9 / 5) - 459.67;
+                        }
+            
+
+         // Time Converter
+                            public static double secondsToMilliseconds(double seconds) {
+                                return seconds * 1000;
+                            }
+                        
+                            public static double secondsToMinutes(double seconds) {
+                                return seconds / 60;
+                            }
+                        
+                            public static double minutesToSeconds(double minutes) {
+                                return minutes * 60;
+                            }
+                        
+                            public static double hoursToMinutes(double hours) {
+                                return hours * 60;
+                            }
+                        
+                            public static double daysToHours(double days) {
+                                return days * 24;
+                            }
+                        
+                            public static double weeksToDays(double weeks) {
+                                return weeks * 7;
+                            }
+                        
+                            public static double yearsToDays(double years) {
+                                return years * 365.25; // Allowing for leap years
+                            }
+                        
+      
+        // SpeedConverter
+                                public static double metersPerSecondToKilometersPerHour(double metersPerSecond) {
+                                    return metersPerSecond * 3.6;
+                                }
+                            
+                                public static double kilometersPerHourToMetersPerSecond(double kilometersPerHour) {
+                                    return kilometersPerHour / 3.6;
+                                }
+                            
+                                public static double milesPerHourToKilometersPerHour(double milesPerHour) {
+                                    return milesPerHour * 1.60934;
+                                }
+                            
+                                public static double kilometersPerHourToMilesPerHour(double kilometersPerHour) {
+                                    return kilometersPerHour / 1.60934;
+                                }
+
+        // AngleConverter
+                                    public static double degreesToRadians(double degrees) {
+                                        return degrees * Math.PI / 180;
+                                    }
+                                
+                                    public static double radiansToDegrees(double radians) {
+                                        return radians * 180 / Math.PI;
+                                    }
+
 
 
            //Power
@@ -92,34 +214,42 @@ public class Calculator {
          //natural logarithm
     public double naturalLogarithm(double value) {
         return Math.log(value);
-    }
+       }
+
+
 
         //Inverse of Trignometric functions 
     
+        public static double inverseSin(double value) {
+            return Math.asin(value);
+        }
+    
+        public static double inverseCos(double value) {
+            return Math.acos(value);
+        }
+    
+        public static double inverseTan(double value) {
+            return Math.atan(value);
+        }
+    
+        public static double inverseSec(double value) {
+            return 1 / Math.acos(value);
+        }
+    
+        public static double inverseCot(double value) {
+            return 1 / Math.atan(value);
+        }
+    
+        public static double inverseCsc(double value) {
+            return 1 / Math.asin(value);
+        }
+        
 
-    public static double inverseSin(double value) {
-        return Math.asin(value);
     }
+    
+       
 
-    public static double inverseCos(double value) {
-        return Math.acos(value);
-    }
 
-    public static double inverseTan(double value) {
-        return Math.atan(value);
-    }
 
-    public static double inverseSec(double value) {
-        return 1 / Math.acos(value);
-    }
 
-    public static double inverseCot(double value) {
-        return 1 / Math.atan(value);
-    }
-
-    public static double inverseCsc(double value) {
-        return 1 / Math.asin(value);
-    }
-
-}
 
