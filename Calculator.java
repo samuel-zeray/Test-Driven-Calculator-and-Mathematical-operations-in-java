b6172850c1702000a6ff23514bf947f988336b90
Tewelde-gebre <teweldegebre71@gmail.com>
2024-05-17 07:37:33 -0200

    add a comment area of cylinder fun in java

--- Calculator.java
+++ Calculator.java
@@ -432,6 +432,18 @@ public class AntilogCalculator {
                              }
 
 
+// Cylinder 
+ 
+
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * (radius + height);
+   
+}
     }   //End 
 
     
